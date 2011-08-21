43d22dcbb9b3a48d73fc865fd5c8b6fc722d463a
Francois Laberge <francoislaberge@gmail.com>
2011-08-21 14:36:52 -0700

    BIG-CHECKIN: Added module dependency file. Added Express, Less, ejs modules. Added a basic layout template a template for the home pages's content. Added favicon

--- server/app.js
+++ server/app.js
@@ -1,7 +1,51 @@
-var http = require('http')
-    port = 80;
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('GamesJS. Coming Soon.\n');
-}).listen(port);
+// Dependencies
+var express = require('express');
+
+// Create App/Server
+var app = module.exports = express.createServer();
+
+// ----------------
+// Configurations
+// ----------------
+var port = 8080;
+  // General
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+  
+  
+  
+});
+  // Development specific 
+app.configure('development', function(){
+  app.use(express.static(__dirname + '/../www'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  console.log('Running in Development Mode');
+});
+  // Production specific
+app.configure('production', function(){
+  port = 80;
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/../www', { maxAge: oneYear }));
+  app.use(express.errorHandler());
+console.log('Running in Production Mode');
+});
+
+// Set EJS as our default Template Engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+
+
+
+// Routes
+  // Home Page
+app.get('/', function(req,res){
+  res.render('index');
+});
+
+// Start the server
+app.listen(port);
 console.log('Server running on port '+port);
